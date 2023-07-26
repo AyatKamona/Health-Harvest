d56af93d246dcdf6af1c8833e33c78fe10b53cd0
Karansingh Chauhan <c2karansingh@gmail.com>
2023-07-25 22:19:51 -0300

    store name in shared pred and redirect to dashboard if already set

--- HealthHarvest/app/src/main/java/csci5708/mobilecomputing/healthharvest/UserOnboarding.kt
+++ HealthHarvest/app/src/main/java/csci5708/mobilecomputing/healthharvest/UserOnboarding.kt
@@ -3,6 +3,7 @@ package csci5708.mobilecomputing.healthharvest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,16 +13,32 @@ class UserOnboarding : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_onboarding)
 
-        var userName: EditText = findViewById(R.id.userName)
-        var nextButton: Button = findViewById(R.id.nextButton)
+        val userName: EditText = findViewById(R.id.userName)
+        val nextButton: Button = findViewById(R.id.nextButton)
+
+        // start the user dashboard activity if the user name is already set
+        val sharedPref = getSharedPreferences("user", MODE_PRIVATE)
+        val user = sharedPref.getString("userName", null)
+        Log.d("health-harvest", "onCreate: $user")
+        if(user != null) {
+            val intent = Intent(this, UserDashboard::class.java)
+            startActivity(intent)
+        }
 
         nextButton.setOnClickListener {
             if(userName.text.toString().isEmpty()) {
                 Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            // set the user name in shared preferences
+            val sharedPrefStorage = getSharedPreferences("user", MODE_PRIVATE)
+            val editor = sharedPrefStorage.edit()
+            editor.putString("userName", userName.text.toString())
+
+            editor.apply()
+
             val intent = Intent(this, UserDashboard::class.java)
-            intent.putExtra("userName", userName.text.toString())
             startActivity(intent)
         }
     }
