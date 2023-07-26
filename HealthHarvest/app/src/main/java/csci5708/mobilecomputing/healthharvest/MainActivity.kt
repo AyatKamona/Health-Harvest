e79eb9bb2811a4c36f37ff27726ab9deff937bff
Karansingh Chauhan <c2karansingh@gmail.com>
2023-07-26 01:17:18 -0300

    persistent notification for water tracking

--- HealthHarvest/app/src/main/java/csci5708/mobilecomputing/healthharvest/MainActivity.kt
+++ HealthHarvest/app/src/main/java/csci5708/mobilecomputing/healthharvest/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import csci5708.mobilecomputing.healthharvest.services.WaterCounterNotificationService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val serviceIntent = Intent(this, WaterCounterNotificationService::class.java)
+        startService(serviceIntent)
 
         // start the user dashboard activity if the user name is already set
         val sharedPref = getSharedPreferences("user", MODE_PRIVATE)
